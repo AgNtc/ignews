0168474f6189b36adaef82379ea7bc6cad90a292
Agntc <aguidobispo@outlook.com>
2022-05-02 15:50:55 -0300

    integração do prismic funcionando

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,17 +1,31 @@
 import { SignInButton } from '../SignInButton';
+import Link from 'next/link';
+
 import styles from './styles.module.scss';
+import { useRouter } from 'next/router';
+import { ActiveLink } from '../ActiveLink';
+
 export const Header= () => {
+  const { asPath } = useRouter();
+
+  console.log(asPath);
+  
+
     return (
       <header className={styles.headerContainer}>
         <div className={styles.headerContent}>
           <img src="/images/logo.svg" alt="ig.news" />
           <nav>
-            <a href="" className={styles.active}>
-              Home
-            </a>
-            <a href="">
-              Posts
-            </a>
+            <ActiveLink activeClassName={styles.active}href='/'>
+              <a>
+                Home
+              </a>
+            </ActiveLink>
+            <ActiveLink activeClassName={styles.active}href='/posts' prefetch>
+              <a>
+                Posts
+              </a>
+            </ActiveLink>
             
           </nav>
             <SignInButton />
